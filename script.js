fe313f101dbb2670e10fa56c1df2cb1747c881f2
Billan Abdi <billanabdi04@gmail.com>
2023-11-20 16:33:04 +0400

    Sloution

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+
+function getFirstNames(names) {
+  const names =
+    (["Ahmed", "Aisha", "Amina"],
+    ["Jamac", "Khadija", "Mohamed"],
+    ["Mohamed", "Ali"]);
+  for (let i = 0; i < names.length; i++) {
+    return [i][0];
+  }
+}
+
+console.log(getFirstNames);
